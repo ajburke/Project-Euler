65490b4b37c4f3ba93231044b3f7f0f6f2056ab8
AJ Burke <alexander.burke@taxware.com>
2013-11-07 15:58:57 -0500

    Can now take unlimited arguements, wont hang up on non-numerics

--- Project1.rb
+++ Project1.rb
@@ -3,52 +3,48 @@ class Problem1
 	def initialize(*args)
 		$numbersToCheck = *args
 		$total = 0
-	end
-=begin
-	def returnSumOfMultiples
-		$numbersToCheck.each do |number|
-			getSumOfMultiples(number)
-		end
-		return $total
+		$multipleList = Array.new
 	end
 
-	def getSumOfMultiples(baseNumber)
-		(1..(1000-1)).each do |number|
-			addMultipleIfItIsAMultipleOfBaseNumber(number, baseNumber)
-		end
-	end
-
-	def addMultipleIfItIsAMultipleOfBaseNumber(number, baseNumber)
-		if((number%baseNumber == 0) && (number%3 != 0))
-			$total += number
-		end
-	end
-=end
 	def returnTotal
-		addMultiplesofThree
-		addMultiplesofFive
+		removeNonNumerics
+		findMultiplesOf($numbersToCheck)
+		sumMultipleList
 		return $total
 	end
 
-	def addMultiplesofThree
-		counter = 3
-		while counter < 1000 do
-			$total += counter
-			counter += 3
-		end 
+	def removeNonNumerics
+		$numbersToCheck.each do |number|
+			if $numbersToCheck[number].is_a?(Numeric)
+				#leave number in the list of numbers to check
+				else
+					$numbersToCheck.delete_at(number)
+			end
+		end
 	end
 
-	def addMultiplesofFive
-		counter = 5
-		while counter < 1000 do
-			addToTotalIfNotAMultipleOfThree(counter)
-			counter += 5
-		end 
+	def findMultiplesOf(numbers)
+
+		$numbersToCheck.each do | number |
+			counter = number
+			while counter < 1000 do
+				addToMultipleArrayIfNotAlreadyIncluded(counter)
+				counter += number
+			end 
+		end
 	end
 
-	def addToTotalIfNotAMultipleOfThree(number)
-		if(number%3 != 0)
-			$total += number
+	def addToMultipleArrayIfNotAlreadyIncluded(number)
+		if $multipleList.include?(number)
+			#if the multiple is already in the list, dont do anything
+			else
+				$multipleList << number
+		end
+	end
+
+	def sumMultipleList
+		$multipleList.each do |multiple|
+			$total += multiple
 		end
 	end
 
