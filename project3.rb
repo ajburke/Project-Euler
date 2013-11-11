7979af6d4d0f43c15fdd16c188a4738b3f17eaf4
ajburke <alexander.burke@taxware.com>
2013-11-11 15:45:12 -0500

    commit with final solution

--- project3.rb
+++ project3.rb
@@ -1,56 +1,28 @@
-class Project3
+BFN = 600851475143
 
-	PrimeList = [2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47, 53, 59, 61, 67, 71, 73, 79, 83, 89, 97]
-	
-	
-	def initialize
-		@currentPrime = 2
-		@largestFactor = 2
-	end
+#solution taken from http://www.mathblog.dk/project-euler-problem-3/
+#adapted into ruby by myself
+#All credit for algorithm goes to author of said blog
 
-	def isFactor?(number, factor)
-		if number % factor == 0
-			updateLargestFactor(factor)
-			return true
+def getLargestPrimeFactor
+
+	counter = 2
+	newnumm = BFN
+	largestFactor = 0
+	loop do
+		if (newnumm%counter == 0)
+			newnumm = newnumm/counter
+			largestFactor = counter
 		else
-			return false
+			counter = counter + 1
 		end
+		break if (counter * counter > newnumm)
 	end
-
-	def getNextPrimeFromList
-		@currentPrime = PrimeList[PrimeList.index(@currentPrime) + 1]
+	if newnumm > largestFactor
+		largestFactor = newnumm
 	end
-
-	def getNextPrimeAfter(number)
-		return
-	end
-
-	def updateLargestFactor(factor)
-		if factor > @largestFactor
-			@largestFactor = factor
-		end
-	end
-
-	def solve(number)
-	
-		if isFactor?(number, @currentPrime)
-
-			if number == @currentPrime				
-				return @largestFactor
-			else
-				number = number/@currentPrime
-				@currentPrime = PrimeList[0]
-				return solve(number)
-			end
-		else
-			@currentPrime = getNextPrimeFromList
-			return solve(number)
-		end
-		return @currentPrime
-	end
-
+	puts largestFactor
 end
 
 
-answer = Project3.new
-puts answer.solve(100)
+getLargestPrimeFactor
