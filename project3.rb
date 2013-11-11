29af3ff89e215d21cad0292f3f89db1bd04be028
ajburke <alexander.burke@taxware.com>
2013-11-11 11:34:49 -0500

    refactored algorithm

--- project3.rb
+++ project3.rb
@@ -1,38 +1,56 @@
 class Project3
-	def createArrayofPossiblePrimes(maxNumber)
-		possiblePrimes = Array.new
-		(2..maxNumber).step(2) do |n|
-			possiblePrimes << n
-		end
-		return possiblePrimes
+
+	PrimeList = [2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47, 53, 59, 61, 67, 71, 73, 79, 83, 89, 97]
+	
+	
+	def initialize
+		@currentPrime = 2
+		@largestFactor = 2
 	end
 
-
-
-end
-
-
-
-
-class Test_Project3
-	THENUMBER = 600851475143
-	#THEOTHERNUMBER =  600851475143/2
-	def test_createArrayofPossiblePrimes
-		testProject3 = Project3.new
-		possiblePrimes = testProject3.createArrayofPossiblePrimes(THENUMBER)
-
-		if possiblePrimes.size != (THENUMBER/2)
-			puts  "test_createArrayofPossiblePrimes failure"
+	def isFactor?(number, factor)
+		if number % factor == 0
+			updateLargestFactor(factor)
+			return true
 		else
-			puts "test_createArrayofPossiblePrimes"
+			return false
 		end
 	end
 
-	def setup
-		testProject3 = Project3.new
-		return testProject3
+	def getNextPrimeFromList
+		@currentPrime = PrimeList[PrimeList.index(@currentPrime) + 1]
 	end
+
+	def getNextPrimeAfter(number)
+		return
+	end
+
+	def updateLargestFactor(factor)
+		if factor > @largestFactor
+			@largestFactor = factor
+		end
+	end
+
+	def solve(number)
+	
+		if isFactor?(number, @currentPrime)
+
+			if number == @currentPrime				
+				return @largestFactor
+			else
+				number = number/@currentPrime
+				@currentPrime = PrimeList[0]
+				return solve(number)
+			end
+		else
+			@currentPrime = getNextPrimeFromList
+			return solve(number)
+		end
+		return @currentPrime
+	end
+
 end
 
-test = Test_Project3.new 
-test.test_createArrayofPossiblePrimes
+
+answer = Project3.new
+puts answer.solve(100)
